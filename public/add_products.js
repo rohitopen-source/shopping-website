ee1aeedb9169b022f946ffe1108f6fe7444c42db
Rohit <rohit931999@gmail.com>
2020-05-10 16:32:35 +0530

    updated add_product.js

--- public/add_products.js
+++ public/add_products.js
@@ -1,6 +1,25 @@
 $(function (){
 
      $('#btnProductAdd').click(function (){
-         
+         $(function () {
+    let productName = $('#productName')
+    let productManufacturer = $('#productManufacturer')
+    let productPrice = $('#productPrice')
+    
+    $('#btnProductAdd').click(function () {
+
+        addProduct(
+            productName.val(),
+            productManufacturer.val(),
+            productPrice.val(),
+            function (addedProduct) {
+                window.alert("Added " + addedProduct.name + " to Database")
+            }
+        )
+
+
+    })
+
+})
      })
 })
